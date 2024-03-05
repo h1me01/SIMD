a52d1c3265dfe6b281c93e546667a528d3046124
h1me01 <semih.oezalp@hak-feldkirch.at>
2024-03-05 15:11:49 +0100

    small adjustments

--- main.cpp
+++ main.cpp
@@ -7,10 +7,7 @@ int main() {
      * TESTING
      */
     const int MAX_ITERATIONS = 1000;
-
-    std::vector<double> slow;
-    std::vector<double> sse;
-    std::vector<double> avx;
+    std::vector<double> slow, sse, avx;
 
     int count = 0;
     while(count < MAX_ITERATIONS) {
@@ -18,23 +15,18 @@ int main() {
         std::vector<double> tempAdam = getAdamResult();
         std::vector<double> tempGradientsUpdate = getGradientsUpdateResult();
 
-        slow.push_back(tempDot[0]);
-        slow.push_back(tempAdam[0]);
-        slow.push_back(tempGradientsUpdate[0]);
-
-        sse.push_back(tempDot[1]);
-        sse.push_back(tempAdam[1]);
-        sse.push_back(tempGradientsUpdate[1]);
-
-        avx.push_back(tempDot[2]);
-        avx.push_back(tempAdam[2]);
-        avx.push_back(tempGradientsUpdate[2]);
+        slow.insert(slow.end(), {tempDot[0], tempAdam[0], tempGradientsUpdate[0]});
+        sse.insert(sse.end(), {tempDot[1], tempAdam[1], tempGradientsUpdate[1]});
+        avx.insert(avx.end(), {tempDot[2], tempAdam[2], tempGradientsUpdate[2]});
 
         count++;
     }
 
+    std::cout << "Slow: ";
     std::cout << calculateAverage(slow) << std::endl;
+    std::cout << "SSE: ";
     std::cout << calculateAverage(sse) << std::endl;
+    std::cout << "AVX: ";
     std::cout << calculateAverage(avx) << std::endl;
 
     return 0;
